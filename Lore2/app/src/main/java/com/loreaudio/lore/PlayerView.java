4cbfb77e420885f13b2bf3a49c45cd7045cdc1e7
Sunshine Chong <sunshinechong@gmail.com>
2018-03-24 17:39:23 -0700

    created player activity and layout

---
+++ Lore2/app/src/main/java/com/loreaudio/lore/PlayerView.java
@@ -0,0 +1,15 @@
+package com.loreaudio.lore;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlayerView extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player_view);
+    }
+}
