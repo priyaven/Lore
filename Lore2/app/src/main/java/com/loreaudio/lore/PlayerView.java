276f5404915a2e5ef0afb1f2ce7866f6b4f6eb3f
Priya Venkateshan <prvenkateshan@ebay.com>
2018-03-30 16:14:15 -0700

    Made play/pause button work.

--- Lore2/app/src/main/java/com/loreaudio/lore/PlayerView.java
+++ Lore2/app/src/main/java/com/loreaudio/lore/PlayerView.java
@@ -1,19 +1,3 @@
-/*package com.loreaudio.lore;
-
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-public class PlayerView extends AppCompatActivity
-{
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_player_view);
-    }
-}
-*/
 package com.loreaudio.lore;
 
 import android.content.ActivityNotFoundException;
@@ -24,6 +8,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.ServiceConnection;
 import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
@@ -41,97 +26,36 @@ import android.widget.TextView;
 import android.widget.MediaController.MediaPlayerControl;
 import android.widget.Toast;
 
-import android.widget.ProgressBar;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+
 import android.util.Log;
 
 public class PlayerView extends AppCompatActivity implements MediaPlayerControl {
-
-    private MusicController controller;
-    private MusicService musicSrv;
-    private Intent playIntent;
-    private boolean musicBound=false;
-    private ArrayList<String> songList;
-
-    private boolean mDragging;
 
     Story curStory;
     int curPosition = 1;
     int prevPosition = 0;
     Chapter curChapter;
     Chapter prevChapter;
-
-
+    private MusicController controller;
+    private MusicService musicSrv;
+    private Intent playIntent;
+    private boolean musicBound = false;
+    private ArrayList<String> songList;
+    private boolean mDragging;
     private SeekBar mProgress;
-    private Handler mHandler ;
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_player_view);
-
-        curStory = (Story) getIntent().getSerializableExtra("CurStory");
-        curPosition = (int) getIntent().getIntExtra("CurPosition", 1);
-        prevPosition = (int) getIntent().getIntExtra("PrevPosition", 0);
-
-        ImageView imgview = (ImageView) findViewById(R.id.coverImage);
-        int imgsrc = getResources().getIdentifier(curStory.getImgfile(), null, getPackageName());
-        Drawable resimg = getResources().getDrawable(imgsrc);
-        imgview.setBackground(resimg);
-        fix_chapter();
-
-
-        TextView storyTitle = (TextView) findViewById(R.id.bookTitle) ;
-        storyTitle.setText(curStory.getTitle());
-
-        TextView authorName = (TextView) findViewById(R.id.authorName);
-        authorName.setText(curStory.getAuthor());
-
-        setController();
-
-        mProgress = (SeekBar) findViewById(R.id.seekBar);
-        mProgress.setOnSeekBarChangeListener(mSeekListener);
-        mProgress.setMax(100);
-
-
-        // Register mMessageReceiver to receive messages.
-        LocalBroadcastManager.getInstance(this).registerReceiver(mMessageReceiver,
-                new IntentFilter("audio-done"));
-
-        //Make sure you update Seekbar on UI thread
-        mHandler = new Handler();
-        PlayerView.this.runOnUiThread(new Runnable() {
-
-            @Override
-            public void run() {
-                if ((musicSrv!= null) && (musicSrv.isPng())) {
-                        int position = getCurrentPosition();
-                        int duration = getDuration();
-                        if (mProgress != null) {
-                            if (duration > 0) {
-                                // use long to avoid overflow
-                                long pos = 100L * position / duration;
-                                mProgress.setProgress((int) pos);
-
-                            }
-                        }
-                    }
-                mHandler.postDelayed(this, 100);
-            }
-        });
-
-    }
+    private Handler mHandler;
+    private ImageButton playPauseButton;
 
     private BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             // Extract data included in the Intent
-            if(!curChapter.isEnd()) {
+            if (!curChapter.isEnd()) {
                 int audiotype = intent.getIntExtra("audiotype", 999);
                 if (audiotype == 0) {
                     musicSrv.playSong(1);
@@ -141,64 +65,11 @@ public class PlayerView extends AppCompatActivity implements MediaPlayerControl 
             }
         }
     };
-
-
-    private void fix_chapter(){
-        prevChapter = curChapter;
-        curChapter = curStory.chapters.get(new Integer(curPosition));
-        songList = new ArrayList<String>();
-        songList.add(curChapter.mp3File);
-        songList.add(curChapter.mp3QuestionFile);
-
-        TextView chapterTitle = (TextView) findViewById(R.id.chapterTitle);
-        chapterTitle.setText(curChapter.getTitle());
-
-        ImageButton prev = (ImageButton) findViewById(R.id.prev_ch);
-        prev.setEnabled(true);
-
-        if (curChapter.isEnd()){
-            ImageButton skip = (ImageButton) findViewById(R.id.skip);
-            skip.setEnabled(false);
-            //Button pause = (Button) findViewById(R.id.playpause);
-            //pause.setEnabled(false);
-        }
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        if(playIntent==null){
-            playIntent = new Intent(this, MusicService.class);
-            bindService(playIntent, musicConnection, Context.BIND_AUTO_CREATE);
-            startService(playIntent);
-        }
-    }
-
-    private void setController(){
-        //set the controller up
-        controller = new MusicController(this);
-        controller.setPrevNextListeners(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                playNext();
-            }
-        }, new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                playPrev();
-            }
-        });
-
-        controller.setMediaPlayer(this);
-        controller.setAnchorView(findViewById(R.id.seekBar));
-        controller.setEnabled(true);
-    }
-
-    private ServiceConnection musicConnection = new ServiceConnection(){
+    private ServiceConnection musicConnection = new ServiceConnection() {
 
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
-            MusicService.MusicBinder binder = (MusicService.MusicBinder)service;
+            MusicService.MusicBinder binder = (MusicService.MusicBinder) service;
             //get service
             musicSrv = binder.getService();
             //pass list
@@ -214,22 +85,12 @@ public class PlayerView extends AppCompatActivity implements MediaPlayerControl 
 
 
     };
-
-    /* seek bar stuff from https://github.com/brightec/ExampleMediaController/blob/master/src/uk/co/brightec/example/mediacontroller/VideoControllerView.java
-
-     */
-
     private OnSeekBarChangeListener mSeekListener = new OnSeekBarChangeListener() {
 
         public void onStartTrackingTouch(SeekBar bar) {
 
             mDragging = true;
 
-            // By removing these pending progress messages we make sure
-            // that a) we won't update the progress while the user adjusts
-            // the seekbar and b) once the user is done dragging the thumb
-            // we will post one of these messages to the queue again and
-            // this ensures that there will be exactly one message queued up.
         }
 
         public void onProgressChanged(SeekBar bar, int pos, boolean fromuser) {
@@ -262,11 +123,134 @@ public class PlayerView extends AppCompatActivity implements MediaPlayerControl 
         }
     };
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player_view);
+
+        curStory = (Story) getIntent().getSerializableExtra("CurStory");
+        curPosition = (int) getIntent().getIntExtra("CurPosition", 1);
+        prevPosition = (int) getIntent().getIntExtra("PrevPosition", 0);
+
+        ImageView imgview = (ImageView) findViewById(R.id.coverImage);
+        int imgsrc = getResources().getIdentifier(curStory.getImgfile(), null, getPackageName());
+        Drawable resimg = getResources().getDrawable(imgsrc);
+        imgview.setBackground(resimg);
+        fix_chapter();
+
+
+        TextView storyTitle = (TextView) findViewById(R.id.bookTitle);
+        storyTitle.setText(curStory.getTitle());
+
+        TextView authorName = (TextView) findViewById(R.id.authorName);
+        authorName.setText(curStory.getAuthor());
+
+        setController();
+
+        mProgress = (SeekBar) findViewById(R.id.seekBar);
+        mProgress.setOnSeekBarChangeListener(mSeekListener);
+        mProgress.setMax(100);
+
+        playPauseButton = (ImageButton) findViewById(R.id.playpause);
+
+
+        // Register mMessageReceiver to receive messages.
+        LocalBroadcastManager.getInstance(this).registerReceiver(mMessageReceiver,
+                new IntentFilter("audio-done"));
+
+        //Make sure you update Seekbar on UI thread
+        mHandler = new Handler();
+        PlayerView.this.runOnUiThread(new Runnable() {
+
+            @Override
+            public void run() {
+                if ((musicSrv != null)) {
+                    if (musicSrv.isPng()) {
+                        int position = getCurrentPosition();
+                        int duration = getDuration();
+                        if (mProgress != null) {
+                            if (duration > 0) {
+                                // use long to avoid overflow
+                                long pos = 100L * position / duration;
+                                mProgress.setProgress((int) pos);
+
+                            }
+                        }
+                        if (playPauseButton != null){
+                            playPauseButton.setBackgroundResource(R.drawable.pause);
+                        }
+                    }
+                    else if (playPauseButton != null) {
+                        playPauseButton.setBackgroundResource(R.drawable.play);
+                    }
+                }
+                mHandler.postDelayed(this, 1000);
+            }
+        });
+
+    }
+
+    private void fix_chapter() {
+        prevChapter = curChapter;
+        curChapter = curStory.chapters.get(new Integer(curPosition));
+        songList = new ArrayList<String>();
+        songList.add(curChapter.mp3File);
+        songList.add(curChapter.mp3QuestionFile);
+
+        TextView chapterTitle = (TextView) findViewById(R.id.chapterTitle);
+        chapterTitle.setText(curChapter.getTitle());
+
+        ImageButton prev = (ImageButton) findViewById(R.id.prev_ch);
+        prev.setEnabled(true);
+
+        if (curChapter.isEnd()) {
+            ImageButton skip = (ImageButton) findViewById(R.id.skip);
+            skip.setEnabled(false);
+            //Button pause = (Button) findViewById(R.id.playpause);
+            //pause.setEnabled(false);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (playIntent == null) {
+            playIntent = new Intent(this, MusicService.class);
+            bindService(playIntent, musicConnection, Context.BIND_AUTO_CREATE);
+            startService(playIntent);
+        }
+    }
+
+    /* seek bar stuff from
+    https://github.com/brightec/ExampleMediaController/blob/master/src/uk/co/brightec/example/mediacontroller/VideoControllerView.java
+
+     */
+
+    private void setController() {
+        //set the controller up
+        controller = new MusicController(this);
+        controller.setPrevNextListeners(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playNext();
+            }
+        }, new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playPrev();
+            }
+        });
+
+        controller.setMediaPlayer(this);
+        controller.setAnchorView(findViewById(R.id.seekBar));
+        controller.setEnabled(true);
+    }
+
     private int setProgress() {
         //if (mDragging) {
         //    return 0;
         //}
-        if (!musicSrv.playing){
+        if (!musicSrv.playing) {
             return 0;
         }
         int position = getCurrentPosition();
@@ -275,7 +259,7 @@ public class PlayerView extends AppCompatActivity implements MediaPlayerControl 
             if (duration > 0) {
                 // use long to avoid overflow
                 long pos = 100L * position / duration;
-                mProgress.setProgress( (int) pos);
+                mProgress.setProgress((int) pos);
             }
             //int percent = mPlayer.getBufferPercentage();
             //mProgress.setSecondaryProgress(percent * 10);
@@ -293,15 +277,27 @@ public class PlayerView extends AppCompatActivity implements MediaPlayerControl 
 
     /* end seek bar stuff */
 
+    public void updatePausePlay() {
+        if((musicSrv == null) || (playPauseButton == null)) {
+            return;
+        }
+
+        if (musicSrv.isPng()) {
+            playPauseButton.setBackgroundResource(R.drawable.pause);
+        } else {
+            playPauseButton.setBackgroundResource(R.drawable.play);
+        }
+    }
+
 
     //play next
-    private void playNext(){
+    private void playNext() {
         musicSrv.playNext();
         controller.show(0);
     }
 
     //play previous
-    private void playPrev(){
+    private void playPrev() {
         musicSrv.playPrev();
         controller.show(0);
     }
@@ -365,7 +361,7 @@ public class PlayerView extends AppCompatActivity implements MediaPlayerControl 
     }
 
 
-    public void songpicked(View view){
+    public void songpicked(View view) {
         musicSrv.playSong(0);
     }
 
@@ -374,7 +370,24 @@ public class PlayerView extends AppCompatActivity implements MediaPlayerControl 
 
     }
 
-    private void yesNoListener(){
+    public void togglePlay(View view){
+        if(musicSrv == null){
+            return;
+        }
+        if(musicSrv.isPng()){
+            musicSrv.pausePlayer();
+        } else {
+            musicSrv.go();
+        }
+        updatePausePlay();
+    }
+
+    public void toggleBookmark(View view){
+        ImageButton bookmark = (ImageButton) findViewById(R.id.bookmark_button);
+
+    }
+
+    private void yesNoListener() {
         Intent intent = new Intent(
                 RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
 
@@ -390,6 +403,7 @@ public class PlayerView extends AppCompatActivity implements MediaPlayerControl 
         }
 
     }
+
     public void listenToYesNo(View view) {
         yesNoListener();
     }
