35cf545fbfc511b96109ef37c0d6a0fc361e5a68
priyaven <priyaven@gmail.com>
2018-05-05 17:29:12 -0700

    Missed adding file to previous commit.

--- Lore2/app/src/main/java/com/loreaudio/lore/MainActivity.java
+++ Lore2/app/src/main/java/com/loreaudio/lore/MainActivity.java
@@ -4,9 +4,12 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
@@ -29,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homescreen);
         eachstory = (ListView) findViewById(R.id.eachstory);
+        eachstory.setClickable(true);
         //eachstory.setAdapter(new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1,
         //getResources().getStringArray(R.array.stories)));
 
@@ -40,6 +44,13 @@ public class MainActivity extends AppCompatActivity {
             storylist = Story.storyObjects(storyNodes);
             StoryAdapter storyAdapter = new StoryAdapter(MainActivity.this, storylist);
             eachstory.setAdapter(storyAdapter);
+            eachstory.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    // Get the selected item text from ListView
+                    playStory(position);
+                }
+            });
         } catch (SAXException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -50,14 +61,18 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void playNextChapter(View view) {
+   // public void playNextChapter(View view) {
+    public void playStory(int position){
+
         Intent intent = new Intent(this, PlayerView.class);
-        Story firstStory = storylist.get(0);
-        intent.putExtra("CurStory", firstStory);
-        intent.putExtra("CurPosition", 1);
+        Story curStory = storylist.get(position);
+        if(curStory == null){
+            return;
+        }
+        intent.putExtra("CurStory", curStory);
+        intent.putExtra("CurPosition", curStory.getFirstChapterId());
         intent.putExtra("PrevPosition", 0);
         startActivity(intent);
     }
-
 
 }
