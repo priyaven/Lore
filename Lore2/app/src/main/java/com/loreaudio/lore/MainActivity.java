b4eefe28c938a9ce78b674bf5c0b6c0b58f9bdcd
Sunshine Chong <sunshinechong@gmail.com>
2018-05-05 17:52:44 -0700

    added navigation menu, still needs work
    nav menu shows when app is opened which is a bug.

--- Lore2/app/src/main/java/com/loreaudio/lore/MainActivity.java
+++ Lore2/app/src/main/java/com/loreaudio/lore/MainActivity.java
@@ -2,11 +2,21 @@ package com.loreaudio.lore;
 
 import android.content.Intent;
 import android.content.res.Resources;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toolbar;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
@@ -23,12 +33,38 @@ public class MainActivity extends AppCompatActivity {
 
     ListView eachstory;
     ArrayList<Story> storylist;
+    NavigationView navView;
+    DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homescreen);
         eachstory = (ListView) findViewById(R.id.eachstory);
+        eachstory.setClickable(true);
+
+
+        //drawer = (DrawerLayout)findViewById(R.id.drawer_layout);
+        //drawer.closeDrawer(GravityCompat.START);
+
+//        navView = (NavigationView)findViewById(R.id.nav_view);
+//        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//
+//        navView.setNavigationItemSelectedListener(
+//                new NavigationView.OnNavigationItemSelectedListener() {
+//                    @Override
+//                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+//                        // set item as selected to persist highlight
+//                        menuItem.setChecked(true);
+//                        // close drawer when item is tapped
+//                        drawer.closeDrawers();
+//
+//                        // Add code here to update the UI based on the item selected
+//                        // For example, swap UI fragments here
+//
+//                        return true;
+//                    }
+//                });
         //eachstory.setAdapter(new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1,
         //getResources().getStringArray(R.array.stories)));
 
@@ -40,6 +76,14 @@ public class MainActivity extends AppCompatActivity {
             storylist = Story.storyObjects(storyNodes);
             StoryAdapter storyAdapter = new StoryAdapter(MainActivity.this, storylist);
             eachstory.setAdapter(storyAdapter);
+            eachstory.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    // Get the selected item text from ListView
+                    Log.i("listviewclick", "position is " + Integer.toString(position));
+                    playStory(position);
+                }
+            });
         } catch (SAXException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -50,9 +94,9 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void playNextChapter(View view) {
+    public void playStory(int position) {
         Intent intent = new Intent(this, PlayerView.class);
-        Story firstStory = storylist.get(0);
+        Story firstStory = storylist.get(position);
         intent.putExtra("CurStory", firstStory);
         intent.putExtra("CurPosition", 1);
         intent.putExtra("PrevPosition", 0);
