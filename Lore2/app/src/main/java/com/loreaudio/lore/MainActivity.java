e1ded7b2d0f94505c9efd9a243dc1128c0573461
Sunshine Chong <sunshinechong@gmail.com>
2018-05-19 16:29:50 -0700

    added and fixed navigation drawer function

--- Lore2/app/src/main/java/com/loreaudio/lore/MainActivity.java
+++ Lore2/app/src/main/java/com/loreaudio/lore/MainActivity.java
@@ -5,7 +5,9 @@ import android.content.res.Resources;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -44,29 +46,30 @@ public class MainActivity extends AppCompatActivity {
         eachstory = (ListView) findViewById(R.id.eachstory);
         eachstory.setClickable(true);
 
+        drawer = (DrawerLayout)findViewById(R.id.drawer_layout);
 
+        navView = (NavigationView)findViewById(R.id.nav_view);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
 
-        //drawer = (DrawerLayout)findViewById(R.id.drawer_layout);
-        //drawer.closeDrawer(GravityCompat.START);
+        navView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        drawer.closeDrawers();
 
-//        navView = (NavigationView)findViewById(R.id.nav_view);
-//        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//
-//        navView.setNavigationItemSelectedListener(
-//                new NavigationView.OnNavigationItemSelectedListener() {
-//                    @Override
-//                    public boolean onNavigationItemSelected(MenuItem menuItem) {
-//                        // set item as selected to persist highlight
-//                        menuItem.setChecked(true);
-//                        // close drawer when item is tapped
-//                        drawer.closeDrawers();
-//
-//                        // Add code here to update the UI based on the item selected
-//                        // For example, swap UI fragments here
-//
-//                        return true;
-//                    }
-//                });
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        return true;
+                    }
+                });
 
         //eachstory.setAdapter(new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1,
         //getResources().getStringArray(R.array.stories)));
@@ -110,6 +113,16 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra("CurPosition", curStory.getFirstChapterId());
         intent.putExtra("PrevPosition", 0);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawer.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
