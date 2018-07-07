fbebf6bd7738e123bc383ad73e2a4a10b61ff5b6
priyaven <priyaven@gmail.com>
2018-07-07 13:59:00 -0700

    draws a parent and two children.

--- Lore2/app/src/main/java/com/loreaudio/lore/CircleGraphActivity.java
+++ Lore2/app/src/main/java/com/loreaudio/lore/CircleGraphActivity.java
@@ -25,10 +25,15 @@ public class CircleGraphActivity extends AppCompatActivity {
 
     DisplayMetrics displayMetrics = new DisplayMetrics();
     Context context = this;
+    int marginy = 250;
+    int marginx = 125;
+
     int margin = 10;
 
     int buttonsize = 0;
     int ringsize = 0;
+
+    int numButtons = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +55,10 @@ public class CircleGraphActivity extends AppCompatActivity {
         int height = displayMetrics.heightPixels;
         int width = displayMetrics.widthPixels;
 
-        buttonsize = (width - margin)/4;
+        if (height < width)
+            buttonsize = (height)/4;
+        else
+            buttonsize = (width)/4;
         ringsize = 3*buttonsize;
 
         @SuppressLint("WrongViewCast")
@@ -79,9 +87,47 @@ public class CircleGraphActivity extends AppCompatActivity {
         imgv.setLayoutParams(lp2);
         lyt.addView(imgv);
 
-        final Button b2 = new Button(this);
+        /*int[] lox = new int[2];
+        imgv.getLocationInWindow(lox);
+        int imgvX = lox[0];
+        int imgvY = lox[1];*/
 
-        lyt.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+        final Button b2 = new Button(this);
+        final Button b3 = new Button(this);
+
+        b2.setBackgroundDrawable(getResources().getDrawable(R.drawable.circle));
+
+        //size of button
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(buttonsize, buttonsize);
+
+        //x is bottom left edge of ringa
+        //params.leftMargin = imgvX - buttonsize/3;
+        //params.topMargin = imgvY;
+        b2.setLayoutParams(params);
+        b2.setX(width/2  + ringsize/2 -buttonsize/2);
+        b2.setY(height/2 - marginy);
+        lyt.addView(b2);
+
+
+        b2.setText("test1");
+
+        b3.setBackgroundDrawable(getResources().getDrawable(R.drawable.circle));
+
+        //size of button
+        RelativeLayout.LayoutParams params3 = new RelativeLayout.LayoutParams(buttonsize, buttonsize);
+
+        //x is bottom left edge of ringa
+        //params3.leftMargin = imgvX - buttonsize/3;
+        //params3.topMargin = imgvY;
+        b3.setLayoutParams(params3);
+        b3.setX(width/2 - buttonsize/2 - ringsize/2);
+        b3.setY(height/2 - marginy);
+        lyt.addView(b3);
+
+
+        b3.setText("test2");
+
+        /*lyt.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
             public void onGlobalLayout() {
                 lyt.getViewTreeObserver().removeGlobalOnLayoutListener(this);
 
@@ -101,9 +147,25 @@ public class CircleGraphActivity extends AppCompatActivity {
                 b2.setLayoutParams(params);
                 lyt.addView(b2);
 
+
                 b2.setText("test");
+
+                b3.setBackgroundDrawable(getResources().getDrawable(R.drawable.circle));
+
+                //size of button
+                RelativeLayout.LayoutParams params3 = new RelativeLayout.LayoutParams(buttonsize, buttonsize);
+
+                //x is bottom left edge of ringa
+                params3.leftMargin = x - buttonsize/3;
+                params3.topMargin = y;
+                b2.setLayoutParams(params3);
+                lyt.addView(b3);
+
+
+                b3.setText("reee");
             }
         });
+        */
 
 
     }
